c7a3603c38dde33b9b1c20bd68089b5ad0b37742
BillXu <xuwenyanbill@gmail.com>
2020-08-21 15:55:03 +0800

    fix bug

--- Robot/MatchModule.ts
+++ Robot/MatchModule.ts
@@ -85,7 +85,11 @@ export class MatchModule extends IClientModule
         else if ( eMsgType.MSG_INFORM_PLAYER_ENTER_MATCH_DESK == msgID )
         {
             // svr : { deskID : 234 , port : eMsgPort , token : 2345 } 
-            this.sendMsg( eMsgType.MSG_PLAYER_ENTER_MATCH_DESK , { token : msg[key.token] }, msg[key.port], msg[key.deskID] ) ;
+            let self = this ;
+            setTimeout(() => {
+                self.sendMsg( eMsgType.MSG_PLAYER_ENTER_MATCH_DESK , { token : msg[key.token] }, msg[key.port], msg[key.deskID] ) ;
+            }, 400 );
+            
             return true ;
         }
         return false ;
